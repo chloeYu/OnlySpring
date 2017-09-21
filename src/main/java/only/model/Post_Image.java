6f3ee50986819ac89e87db661916f40436adb3cd
Chloe <dbqhrud4@gmail.com>
2017-09-21 19:46:14 +0900

    Add Post_Image.java by BK @17.09.21

---
+++ src/main/java/only/model/Post_Image.java
@@ -0,0 +1,25 @@
+package only.model;
+
+public class Post_Image {
+	private int pid;
+	private String url;
+	private int seq;
+	public int getPid() {
+		return pid;
+	}
+	public void setPid(int pid) {
+		this.pid = pid;
+	}
+	public String getUrl() {
+		return url;
+	}
+	public void setUrl(String url) {
+		this.url = url;
+	}
+	public int getSeq() {
+		return seq;
+	}
+	public void setSeq(int seq) {
+		this.seq = seq;
+	}	
+}
