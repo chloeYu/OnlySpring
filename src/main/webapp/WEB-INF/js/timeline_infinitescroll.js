4e66ad083c8c89fd885932c066f547fcde414b69
Chloe <dbqhrud4@gmail.com>
2017-09-22 17:49:35 +0900

    Add infinite_scroll by BK @17.09.22

---
+++ src/main/webapp/WEB-INF/js/timeline_infinitescroll.js
@@ -0,0 +1,100 @@
+$(document).ready(function() {
+	
+	const postList = document.getElementById('postList');
+/*	const postListPagination = document.getElementById('post-list-pagination');*/
+	let page = 0;
+
+	addPage(++page);
+	window.onscroll = function() {
+	if (getScrollTop() < getDocumentHeight() - window.innerHeight) return;
+		addPage(++page);
+	};
+	function getPageId(n) {	
+		return 'post-page-' + n;
+	}
+
+	function getDocumentHeight() {
+		const body = document.body;
+		const html = document.documentElement;
+	
+		return Math.max(
+				body.scrollHeight, body.offsetHeight,
+				html.clientHeight, html.scrollHeight, html.offsetHeight
+		);
+	};
+
+	function getScrollTop() {
+		return (window.pageYOffset !== undefined) ? window.pageYOffset : (document.documentElement || document.body.parentNode || document.body).scrollTop;
+	}
+
+	function getPostImage() {
+		/*const hash = Math.floor(Math.random() * Number.MAX_SAFE_INTEGER);
+		const image = new Image;
+	image.className = 'article-list__item__image article-list__item__image--loading';
+	image.src = 'http://api.adorable.io/avatars/250/' + hash;
+	image.onload = function() {
+		image.classList.remove('article-list__item__image--loading');
+	};
+	
+	return post;*/
+}
+
+function getPost() {
+	const articleImage = getArticleImage();
+	const article = document.createElement('article');
+	article.className = 'article-list__item';
+	article.appendChild(articleImage);
+	
+	return post;
+}
+
+function getPostPage(page, postsPerPage = 10) {
+	console.log("getPostPage called");
+	$.post('loadPost', "pageNum="+page, function(data) {
+		console.log("returnede from loadPost")
+		if (data == null) {
+			console.log("no data to append");
+			$("#postList").html("No Post");
+		} else {
+			console.log("data to append");
+			var postView;
+			for (var i = 0; i < data.length; i++) {
+				console.log(postView);
+				postView = buildPost(data[i]);
+				$("#postList").append(postView);
+			}
+		}
+	});
+}
+
+/*function addPaginationPage(page) {
+	const pageLink = document.createElement('a');
+	pageLink.href = '#' + getPageId(page);
+	pageLink.innerHTML = page;
+	
+	const listItem = document.createElement('li');
+	listItem.className = 'article-list__pagination__item';
+	listItem.appendChild(pageLink);
+	
+	articleListPagination.appendChild(listItem);
+	
+	if (page === 2) {
+		articleListPagination.classList.remove('article-list__pagination--inactive');
+	}
+}*/
+
+function fetchPage(page) {
+	//console.log("getPostPage Result:" + getPostPage(page));
+	//postList.append(getPostPage(page));
+}
+
+function addPage(page) {
+	console.log("add page : " + page);
+	getPostPage(page);
+	//fetchPage(page);
+	//addPaginationPage(page);
+}
+
+});
+
+
