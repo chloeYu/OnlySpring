9ff595086af6772733ba6304257e501354052724
Chloe <dbqhrud4@gmail.com>
2017-09-28 14:35:12 +0900

    Add blog page mapping in onlyContoller by BK @17.09.28

--- src/main/java/only/controller/OnlyController.java
+++ src/main/java/only/controller/OnlyController.java
@@ -13,6 +13,7 @@ import javax.servlet.http.HttpSession;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
@@ -170,5 +171,11 @@ public class OnlyController {
 			session.setAttribute("member", member);
 		}
 		return "profileDone";
+	}
+	
+	@RequestMapping(value="/blog/{owner}")
+	public String blog(@PathVariable String owner, Model model) {
+		model.addAttribute("owner", owner);
+		return "blog/blog";
 	}
 }
