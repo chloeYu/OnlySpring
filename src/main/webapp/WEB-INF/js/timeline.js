e9ab32fd635c068b38961c096a7d539999d76b43
Administrator <Administrator@WIN-CKL5540S7HT>
2017-09-28 16:38:03 +0900

    timeline + google by YJ @17.09.28

--- src/main/webapp/WEB-INF/js/timeline.js
+++ src/main/webapp/WEB-INF/js/timeline.js
@@ -10,7 +10,14 @@ $(function() {
 			$('.img_hide').addClass('img_hidden');
 		}
 	});
+	//지도
+	$('#google').on('click', function() {
+		$("#pac-input").show();
+		$("#map").show();
+		$("#google_button").show();
+	});
 });
+
 // textarea focus일 때 작성폼 열기 끝
 // infinite scroll 구현
 /*
@@ -103,13 +110,6 @@ function buildPost(postData) {
 	return postView;
 }
 
-function writePost(event){
-	console.log(event);
-	var pid = console.log(event.target.id);
-	var comment = document.getElementById("commentText-"+pid).value;
-	console.log(text);
-//	$.post('commentWrite', )
-}
 // 이미지 미리보기
 var sel_files = [];
 
@@ -222,3 +222,75 @@ window.onclick = function(event) {
     }
   }
 }
+
+//지도
+var google_map_place;
+
+function initAutocomplete1() {
+    var map = new google.maps.Map(document.getElementById('map'), {
+    	center: {lat: 36.322473, lng: 127.412501},
+      zoom: 18,
+      mapTypeId: 'roadmap'
+    });
+
+    var input = document.getElementById('pac-input');
+    var searchBox = new google.maps.places.SearchBox(input);
+    /*map.controls[google.maps.ControlPosition.TOP_LEFT].push(input);*/
+    var google_button = document.getElementById('google_button');
+
+    map.addListener('bounds_changed', function() {
+      searchBox.setBounds(map.getBounds());
+    });
+
+    var markers = [];
+
+    searchBox.addListener('places_changed', function() {
+      var places = searchBox.getPlaces();
+      
+      if (places.length == 0) {
+        return;
+      }
+
+      markers.forEach(function(marker) {
+        marker.setMap(null);
+      });
+      markers = [];
+
+      var bounds = new google.maps.LatLngBounds();
+      places.forEach(function(place) {
+        if (!place.geometry) {
+          console.log("Returned place contains no geometry");
+          return;
+        }
+        var icon = {
+          url: place.icon,
+          size: new google.maps.Size(71, 71),
+          origin: new google.maps.Point(0, 0),
+          anchor: new google.maps.Point(17, 34),
+          scaledSize: new google.maps.Size(25, 25)
+        };
+
+        markers.push(new google.maps.Marker({
+          map: map,
+          icon: icon,
+          title: place.name,
+          position: place.geometry.location
+        }));
+        
+        google_map_place = place.name;
+
+        if (place.geometry.viewport) {
+          bounds.union(place.geometry.viewport);
+        } else {
+          bounds.extend(place.geometry.location);
+        }
+      });
+      map.fitBounds(bounds);
+    });
+    google.maps.event.addDomListener(google_button, 'click', function(){
+    		$("#pac-input").hide();
+    		$("#map").hide();
+    		$("#google_button").hide();
+    	  $("#google_place_view").append(google_map_place+" 에서");
+      });
+ }
