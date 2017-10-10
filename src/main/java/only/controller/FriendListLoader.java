f6154c46fea069c7dd9e6c46790caa3bd2166274
Administrator <Administrator@WIN-9E53G5M3O3B>
2017-10-10 15:56:30 +0900

    친구목록 띄우기 정상작동.(SB)

--- src/main/java/only/controller/FriendListLoader.java
+++ src/main/java/only/controller/FriendListLoader.java
@@ -5,7 +5,6 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import only.model.Friendlist;
 import only.service.*;
@@ -23,6 +22,11 @@ public class FriendListLoader {
 	}
 	@RequestMapping("/friendaccpet")
 	public String Friendaccpet(Model model, String member_id, String member_id2) {
+		
+		return "friendList";
+	}
+	@RequestMapping("/friendrefuse")
+	public String Friendrefuse(Model model, String member_id, String member_id2) {
 		
 		return "friendList";
 	}
