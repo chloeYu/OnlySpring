1f7aab2504c0e4bdecdd69c9edabb32b84a4fe93
NT <NT@NT>
2017-10-18 01:47:50 +0900

    채팅 메시지 읽음 처리 수정중.. 김재은

--- src/main/java/only/dao/ChatDAO.java
+++ src/main/java/only/dao/ChatDAO.java
@@ -78,7 +78,7 @@ public class ChatDAO {
 		Connection conn = null;
 		PreparedStatement pstmt = null;
 		ResultSet rs = null;
-		String SQL = "SELECT * FROM CHAT WHERE ((fromID = ? AND toID = ?) OR (fromID = ? AND toID = ?)) AND chatID > (SELECT MAX(chatID) - ? FROM CHAT) ORDER BY chatTime";
+		String SQL = "SELECT * FROM CHAT WHERE ((fromID = ? AND toID = ?) OR (fromID = ? AND toID = ?)) AND chatID > (SELECT MAX(chatID) - ? FROM CHAT WHERE (fromID = ? AND toID = ?) OR (fromID = ? AND toID = ?)) ORDER BY chatTime";
 		try {
 			Class.forName(DRIVER);
 			conn = DriverManager.getConnection(URL, USER, PW);
@@ -90,6 +90,10 @@ public class ChatDAO {
 			pstmt.setString(3, toID);
 			pstmt.setString(4, fromID);
 			pstmt.setInt(5, number);
+			pstmt.setString(6, fromID);
+			pstmt.setString(7, toID);
+			pstmt.setString(8, toID);
+			pstmt.setString(9, fromID);
 			rs = pstmt.executeQuery();
 			chatList = new ArrayList<Chat>();
 			while (rs.next()) {
@@ -132,7 +136,7 @@ public class ChatDAO {
 		PreparedStatement pstmt = null;
 		ResultSet rs = null;
 		System.out.println("Message Submit 작동중");
-		String SQL = "INSERT INTO CHAT VALUES (NULL, ?, ?, ?, NOW(), CONCAT(LEAST(?, ?), '-',GREATEST(?, ?)))";
+		String SQL = "INSERT INTO CHAT VALUES (NULL, ?, ?, ?, NOW(), CONCAT(LEAST(?, ?), '-',GREATEST(?, ?)), 0)";
 		try {
 			Class.forName(DRIVER);
 			conn = DriverManager.getConnection(URL, USER, PW);
@@ -166,5 +170,75 @@ public class ChatDAO {
 		}
 		return -1;
 	}
-
+	
+	public int readChat (String fromID, String toID) {
+		Connection conn = null;
+		PreparedStatement pstmt = null;
+		ResultSet rs = null;
+		System.out.println("readChat 작동중");
+		String SQL = "update chat set chatRead = 1 where (fromID = ? and toID = ?)";
+		try {
+			Class.forName(DRIVER);
+			conn = DriverManager.getConnection(URL, USER, PW);
+			System.out.println(conn);
+			System.out.println("Chat DB 연결완료..");
+			pstmt = conn.prepareStatement(SQL);
+			pstmt.setString(1, toID);
+			pstmt.setString(2, fromID);
+			return pstmt.executeUpdate();
+		} catch (Exception e) {
+			System.out.println(e.getMessage());
+			System.out.println("SQL 쿼리문 에러");
+		} finally {
+			try {
+				if (rs != null)
+					rs.close();
+				if (pstmt != null)
+					pstmt.close();
+				if (conn != null)
+					conn.close();
+			} catch (Exception e) {
+				System.out.println(e.getMessage());
+				System.out.println("DB 닫기 에러");
+			}
+		}
+		return -1; // DB 오류
+	}
+	
+	public int getAllUnreadChat (String userID) {
+		Connection conn = null;
+		PreparedStatement pstmt = null;
+		ResultSet rs = null;
+		System.out.println("getAllUnreadChat 작동중");
+		String SQL = "select count(chatID) from chat where toID = ? and chatRead = 0";
+		try {
+			Class.forName(DRIVER);
+			conn = DriverManager.getConnection(URL, USER, PW);
+			System.out.println(conn);
+			System.out.println("Chat DB 연결완료..");
+			pstmt = conn.prepareStatement(SQL);
+			pstmt.setString(1, userID);
+			rs = pstmt.executeQuery();
+			if(rs.next()) {
+				return rs.getInt("count(chatID)");
+			}
+			return 0;
+		} catch (Exception e) {
+			System.out.println(e.getMessage());
+			System.out.println("SQL 쿼리문 에러");
+		} finally {
+			try {
+				if (rs != null)
+					rs.close();
+				if (pstmt != null)
+					pstmt.close();
+				if (conn != null)
+					conn.close();
+			} catch (Exception e) {
+				System.out.println(e.getMessage());
+				System.out.println("DB 닫기 에러");
+			}
+		}
+		return -1; // DB 오류
+	}
 }
